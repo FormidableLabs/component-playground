1a26786a2bb24ba90492976617068593c84f2438
Liu Yang <zation1@gmail.com>
2015-08-02 14:12:25 +0800

    add previewFirst prop

--- src/playground.jsx
+++ src/playground.jsx
@@ -19,7 +19,9 @@ const ReactPlayground = React.createClass({
     theme: React.PropTypes.string,
     noRender: React.PropTypes.bool,
     es6Console: React.PropTypes.bool,
-    context: React.PropTypes.object
+    context: React.PropTypes.object,
+    lineNumbers: React.PropTypes.bool,
+    previewFirst: React.PropTypes.bool
   },
 
   getDefaultProps() {
@@ -38,7 +40,7 @@ const ReactPlayground = React.createClass({
   },
 
   _handleCodeChange(code) {
-    this.setState({ code });
+    this.setState({code});
   },
 
   _toggleCode() {
@@ -55,46 +57,68 @@ const ReactPlayground = React.createClass({
       `);
     }
 
-    return (
-      <div className={"playground" + (this.props.collapsableCode ? " collapsableCode" : "")}>
-        {this.props.docClass ?
-          <Doc
-            componentClass={this.props.docClass}
-            propDescriptionMap={this.props.propDescriptionMap} />
-          : ""
-        }
-        <div className={"playgroundCode" + (this.state.expandedCode ? " expandedCode" : "")}>
-          <Editor
-            onChange={this._handleCodeChange}
-            className="playgroundStage"
-            codeText={this.state.code}
-            theme={this.props.theme} />
-        </div>
-        {this.props.collapsableCode ?
-          <div className="playgroundToggleCodeBar">
+    var doc;
+    if (this.props.docClass) {
+      doc = <Doc
+        componentClass={this.props.docClass}
+        propDescriptionMap={this.props.propDescriptionMap}/>;
+    }
+
+    var toggleBar;
+    if (this.props.collapsableCode) {
+      toggleBar = (
+        <div className="playgroundToggleCodeBar">
             <span className="playgroundToggleCodeLink" onClick={this._toggleCode}>
               {this.state.expandedCode ? "collapse" : "expand"}
             </span>
-          </div>
-          : ""
-        }
-        <div className="playgroundPreview">
-          {this.props.es6Console ?
-            <EsPreview
-              code={this.state.code}
-              lineNumbers={this.props.lineNumbers}
-              scope={this.props.scope} />
-          :
-          <Preview
-            context={this.props.context}
-            code={this.state.code}
-            scope={this.props.scope}
-            lineNumbers={this.props.lineNumbers}
-            noRender={this.props.noRender} />
-          }
         </div>
+      );
+    }
+
+    var preview = this.props.es6Console ?
+      <div className="playgroundPreview">
+        <EsPreview
+          code={this.state.code}
+          lineNumbers={this.props.lineNumbers}
+          scope={this.props.scope}/>
+      </div> :
+      <div className="playgroundPreview">
+        <Preview
+          context={this.props.context}
+          code={this.state.code}
+          scope={this.props.scope}
+          lineNumbers={this.props.lineNumbers}
+          noRender={this.props.noRender}/>
+      </div>;
+
+    var editor = (
+      <div className={"playgroundCode" + (this.state.expandedCode ? " expandedCode" : "")}>
+        <Editor
+          onChange={this._handleCodeChange}
+          className="playgroundStage"
+          codeText={this.state.code}
+          theme={this.props.theme}/>
       </div>
     );
+
+    var className = 'playground';
+    if (this.props.collapsableCode) {
+      className += ' collapsableCode';
+    }
+
+    return this.props.previewFirst ?
+      <div className={className}>
+        {doc}
+        {preview}
+        {toggleBar}
+        {editor}
+      </div> :
+      <div className={className}>
+        {doc}
+        {editor}
+        {toggleBar}
+        {preview}
+      </div>;
   }
 });
 
