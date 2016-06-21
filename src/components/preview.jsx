473d2913f9fdd8232f3fe45517ef0ef51dd97027
LeoAJ <leo0924@gmail.com>
2016-06-21 16:43:13 -0700

    refactor preview.jsx

--- src/components/preview.jsx
+++ src/components/preview.jsx
@@ -1,128 +1,128 @@
-import React from "react";
+import React, { Component, PropTypes } from "react";
 import ReactDom from "react-dom";
 import ReactDOMServer from "react-dom/server";
 import { transform } from "babel-standalone";
 
-const Preview = React.createClass({
-    propTypes: {
-      code: React.PropTypes.string.isRequired,
-      scope: React.PropTypes.object.isRequired,
-      previewComponent: React.PropTypes.node,
-      noRender: React.PropTypes.bool,
-      context: React.PropTypes.object
-    },
+class Preview extends Component {
 
-    getInitialState() {
-      return {
-        error: null
-      };
-    },
+  static defaultProps = {
+    previewComponent: "div"
+  };
 
-    getDefaultProps() {
-      return {
-        previewComponent: "div"
-      };
-    },
+  static propTypes = {
+    code: PropTypes.string.isRequired,
+    scope: PropTypes.object.isRequired,
+    previewComponent: PropTypes.node,
+    noRender: PropTypes.bool,
+    context: PropTypes.object
+  };
 
-    componentDidMount() {
-      this._executeCode();
-    },
+  state = {
+    error: null
+  };
 
-    componentDidUpdate(prevProps) {
-      clearTimeout(this.timeoutID); //eslint-disable-line
-      if (this.props.code !== prevProps.code) {
-        this._executeCode();
-      }
-    },
+  _compileCode = () => {
+    const { code, context, noRender, scope } = this.props;
+    // const generateContextTypes = (c) => {
+    //   return `{ ${Object.keys(c).map(val =>
+    //     `${val}: React.PropTypes.any.isRequired`).join(", ")} }`;
+    // };
+    const generateContextTypes = function (context) {
+      const keys = Object.keys(context).map(val => `${val}: React.PropTypes.any.isRequired`);
+      return `{ ${keys.join(", ")} }`;
+    };
 
-    _compileCode() {
-      if (this.props.noRender) {
-        const generateContextTypes = function (context) {
-          const keys = Object.keys(context).map(val => `${val}: React.PropTypes.any.isRequired`);
-          return `{ ${keys.join(", ")} }`;
-        };
+    if (noRender) {
+      return transform(`
+        ((${Object.keys(scope).join(", ")}, mountNode) => {
+          class Comp extends React.Component {
 
-        return transform(`
-          (function (${Object.keys(this.props.scope).join(", ")}, mountNode) {
-            return React.createClass({
-              // childContextTypes: { test: React.PropTypes.string },
-              childContextTypes: ${generateContextTypes(this.props.context)},
-              getChildContext: function () { return ${JSON.stringify(this.props.context)}; },
-              render: function () {
-                return (
-                  ${this.props.code}
-                );
-              }
-            });
-          });
-        `, { presets: ["es2015", "react", "stage-1"] }).code;
-      } else {
-        return transform(`
-          (function (${Object.keys(this.props.scope).join(",")}, mountNode) {
-            ${this.props.code}
-          });
-        `, { presets: ["es2015", "react", "stage-1"] }).code;
-      }
-    },
+            getChildContext() {
+              return ${JSON.stringify(this.props.context)};
+            }
 
-    _setTimeout() {
-      clearTimeout(this.timeoutID); //eslint-disable-line no-undef
-      this.timeoutID = setTimeout.apply(null, arguments); //eslint-disable-line no-undef
-    },
-
-    _executeCode() {
-      const mountNode = this.refs.mount;
-
-      try {
-
-        const scope = [];
-
-        for (const s in this.props.scope) {
-          if (this.props.scope.hasOwnProperty(s)) {
-            scope.push(this.props.scope[s]);
+            render() {
+              return (
+                ${this.props.code}
+              );
+            }
           }
-        }
 
-        scope.push(mountNode);
+          Comp.childContextTypes = ${generateContextTypes(this.props.context)};
 
-        const compiledCode = this._compileCode();
-        if (this.props.noRender) {
-        /* eslint-disable no-eval, max-len */
-          const Component = React.createElement(
-            eval(compiledCode).apply(null, scope)
-          );
-          ReactDOMServer.renderToString(React.createElement(this.props.previewComponent, {}, Component));
-          ReactDom.render(
-            React.createElement(this.props.previewComponent, {}, Component),
-            mountNode
-          );
-        } else {
-          eval(compiledCode).apply(null, scope);
-        }
-        /* eslint-enable no-eval, max-len */
-
-        this.setState({
-          error: null
+          return Comp;
         });
-      } catch (err) {
-        this._setTimeout(() => {
-          this.setState({
-            error: err.toString()
-          });
-        }, 500);
-      }
-    },
-
-    render() {
-      return (
-        <div>
-          {this.state.error !== null ?
-            <div className="playgroundError">{this.state.error}</div> :
-            null}
-          <div ref="mount" className="previewArea"/>
-        </div>
-      );
+      `, { presets: ["es2015", "react", "stage-1"] }).code;
+    } else {
+      return transform(`
+        (function (${Object.keys(scope).join(",")}, mountNode) {
+          ${code}
+        });
+      `, { presets: ["es2015", "react", "stage-1"] }).code;
     }
-});
+
+  };
+
+  _setTimeout = (...args) => {
+    clearTimeout(this.timeoutID); //eslint-disable-line no-undef
+    this.timeoutID = setTimeout.apply(null, args); //eslint-disable-line no-undef
+  };
+
+  _executeCode = () => {
+    const mountNode = this.refs.mount;
+    const { scope, noRender, previewComponent } = this.props;
+    const tempScope = [];
+
+    try {
+      Object.keys(scope).forEach(s => tempScope.push(scope[s]));
+      tempScope.push(mountNode);
+      const compiledCode = this._compileCode();
+      if (noRender) {
+        /* eslint-disable no-eval, max-len */
+        const Comp = React.createElement(
+          eval(compiledCode).apply(null, tempScope)
+        );
+        ReactDOMServer.renderToString(React.createElement(previewComponent, {}, Comp));
+        ReactDom.render(
+          React.createElement(previewComponent, {}, Comp),
+          mountNode
+        );
+      } else {
+        eval(compiledCode).apply(null, tempScope);
+      }
+      /* eslint-enable no-eval, max-len */
+
+      this.setState({ error: null });
+    } catch (err) {
+      this._setTimeout(() => {
+        this.setState({ error: err.toString() });
+      }, 500);
+    }
+  };
+
+  componentDidMount = () => {
+    this._executeCode();
+  };
+
+  componentDidUpdate = (prevProps) => {
+    clearTimeout(this.timeoutID); //eslint-disable-line
+    if (this.props.code !== prevProps.code) {
+      this._executeCode();
+    }
+  };
+
+  render() {
+    const { error } = this.state;
+    return (
+      <div>
+        {error !== null ?
+          <div className="playgroundError">{error}</div> :
+          null}
+        <div ref="mount" className="previewArea"/>
+      </div>
+    );
+  }
+
+}
 
 export default Preview;
