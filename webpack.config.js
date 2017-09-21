55850d26a314516a68d2aade1f788bc754c7ac9e
Paula Lavalle <paulathevalley@users.noreply.github.com>
2017-09-21 10:46:00 -0700

    Move loaders for css files to devDependencies (#101)
    * Move loaders to devDeps instead
    
    * Update devDeps to match ecology
    
    * Show me more errors pls
    
    * try npm just for kicks
    
    * thats right i deleted it get out of here yarn
    
    * upgrade firefox launcher cause it worked for ecology
    
    * remove node8 for now and add yarn task just to see what happens
    
    * go back to yarn taking bets on pass or fail
    
    * try upgrading phantomjs to v2
    
    * try replacing phantomjs with phantomjs-prebuilt according to yarn issue
    
    * add back cache in travis
    
    * Add node v8

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: "babel-loader"
-      }, {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      }, {
-        test: /\.(png|jpg)$/,
-        loader: "url-loader?limit=8192"
       }
     ]
   },
